f8478f335e15d8f816cf2f54db5e65d822fcd1bf
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-07 14:42:34 +0530

    Connected backend to webcam

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
+const spawn = require('child_process').spawn
+
+const python = spawn('python', ['./cv.py'])
+
+
 app.use(cors())
 app.get('/', (request, response) => {
-    response.send({id:1,name:"KDD"})
+    python.stdout.on('data', (data) => {
+        console.log(data.toString())
+    })
+    
+    // response.send({id:1,name:"KDD"})
 })
 
 app.listen(port, console.log(`server started at ${port} .....`))
